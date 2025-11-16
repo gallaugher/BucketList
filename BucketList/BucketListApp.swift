41bec7b0ac17c5b8ed30439a4d5304d08c023d6b
gallaugher <john.gallaugher@bc.edu>
2025-11-16 15:13:18 -0500

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,15 @@
+//  BucketListApp.swift
+//  BucketList
+//  Created by John Gallaugher on 11/16/25.
+//  YouTube.com/profgallaugher - gallaugher.bsky.social
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
